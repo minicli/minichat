59082979da3ea6a7b308e8f60a7902e41fd2c76f
erika <efockereinaldo@digitalocean.com>
2020-06-23 11:08:54 +0200

    moving config to separate file, creating a config_example

---
+++ config_example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'app_path' => __DIR__ . '/app/Command',
+    'theme' => '/Unicorn',
+
+    # Twitch Settings
+    # To obtain an OAUTH token for your user, access the chat oauth token generator https://twitchapps.com/tmi/
+    'twitch_user' => 'TWITCH_USERNAME',
+    'twitch_oauth' => 'TWITCH_OAUTH'
+];
